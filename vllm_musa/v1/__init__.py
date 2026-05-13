fe0c50a0c9796bf346fa646ecab95b166fa623df
Joey-gvwal <joey_gvwal@yeah.net>
2026-05-13 19:15:36 +0800

    Avoid eager sparse FlashMLA backend import

--- vllm_musa/v1/__init__.py
+++ vllm_musa/v1/__init__.py
@@ -1,4 +1,3 @@
 import vllm_musa.v1.attention.backends.mla.common
 import vllm_musa.v1.attention.backends.mla.flashmla
-import vllm_musa.v1.attention.backends.mla.flashmla_sparse
 import vllm_musa.v1.spec_decode.utils
