64cedf7d75a4171cbb98c435a4509f35a7df7804
Joey-gvwal <joey_gvwal@yeah.net>
2026-08-07 10:51:04 +0800

    fix(musa): restore optimization contracts on v0.26

---
+++ vllm_musa/optimization_contract/__init__.py
@@ -0,0 +1,27 @@
+from .qwen import matches_qwen35_moe_bf16_prefill_layer
+from .resolver import (
+    bind_optimization_contract,
+    prefers_optimization,
+    resolve_optimization_contract,
+)
+from .types import (
+    ExecutionSignature,
+    ModelFamily,
+    ModelRole,
+    ModelSignature,
+    MusaOptimizationContract,
+    OptimizationFeature,
+)
+
+__all__ = [
+    "ExecutionSignature",
+    "ModelFamily",
+    "ModelRole",
+    "ModelSignature",
+    "MusaOptimizationContract",
+    "OptimizationFeature",
+    "bind_optimization_contract",
+    "matches_qwen35_moe_bf16_prefill_layer",
+    "prefers_optimization",
+    "resolve_optimization_contract",
+]
